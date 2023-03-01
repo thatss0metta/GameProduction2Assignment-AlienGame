02655d13834e23c7ae10cb644328a8b646c420c5
robpietrzak <robpietrzak@yahoo.com>
2023-02-28 19:54:25 -0500

    First push
    Lots o' code n stuff. More to be added later

---
+++ AlienGame/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private StateMachine stateMachine;
+    private NavMeshAgent agent;
+    public NavMeshAgent Agent { get => agent; }
+    // Just for debugging
+    [SerializeField]
+    private string currentState;
+    public Path path;
+    public GameObject player;
+    public Transform playerLoc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stateMachine = GetComponent<StateMachine>();
+        agent = GetComponent<NavMeshAgent>();
+        stateMachine.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
---
+++ AlienGame/Assets/Scripts/Enemy/StateMachine.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public BaseState activeState;
+    public PatrolState patrolState;
+    public ChaseState chaseState;
+    public FieldOfView fov;
+
+    public void Initialize()
+    {
+        patrolState = new PatrolState();
+        chaseState = new ChaseState();
+        ChangeState(patrolState);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activeState != null)
+        {
+            activeState.Perform();
+        }
+        if(fov.canSeePlayer)
+        {
+            ChangeState(chaseState);
+        }
+        else if(!fov.canSeePlayer)
+        {
+            ChangeState(patrolState);
+        }
+    }
+
+    public void ChangeState(BaseState newState)
+    {
+        // Check activeState != null
+        if(activeState != null)
+        {
+            // Run cleanup
+            activeState.Exit();
+        }
+        // Change to new state
+        activeState = newState;
+
+        // Fail-safe null check to make sure new state wasn't null
+        if(activeState != null)
+        {
+            // Setup new state.
+            activeState.stateMachine = this;
+            activeState.enemy = GetComponent<Enemy>();
+            activeState.Enter();
+
+        }
+    }
+}
